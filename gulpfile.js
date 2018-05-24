6e514b8f6dc61da96996a298a9ba6661f452404a
Guram Duka <guram.duka@gmail.com>
2018-05-24 08:00:12 +0300

    added script and patches to build under windows, but not example in index.html works only in chrome

--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,5 @@ const replace = require('gulp-replace');
 gulp.task('compile', function() {
   gulp.src(['webp-decoder.js'])
     .pipe(replace('export', ''))
-    .pipe(gulp.dest('classic'));
+    .pipe(gulp.dest('build'));
 });
